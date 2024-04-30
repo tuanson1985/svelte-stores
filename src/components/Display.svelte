ab818ffde8fea28aa8506b48c40b20c615a12435
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-05-01 03:01:48 +0700

    Custom Stores

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -1,13 +1,23 @@
 <script>
-    //   import { onDestroy } from 'svelte'
-    import { count } from '../stores/stores'
-  
-      // let counter
-  
-      // const unsubscribe = count.subscribe(value => {
-      //   counter = value
-      // })
-      // onDestroy(unsubscribe)
-  </script>
+  //   import { onDestroy } from 'svelte'
+  import { count, customCount } from '../stores/stores'
 
-  <h2>Current count is {$count}</h2>
+  //   let counter
+
+  //   const unsubscribe = count.subscribe(value => {
+  //     counter = value
+  //   })
+
+  //   onDestroy(unsubscribe)
+</script>
+
+<h2>The custom count is {$customCount}</h2>
+
+<button on:click={() => customCount.increment()}>Increment</button>
+<button on:click={() => customCount.increment(5)}>Increment 5</button>
+<button on:click={() => customCount.decrement()}>Decrement</button>
+<button on:click={() => customCount.decrement(5)}>Decrement 5</button>
+<button on:click={customCount.reset}>Reset</button>
+
+<hr />
+<h2>Current count is {$count}</h2>
